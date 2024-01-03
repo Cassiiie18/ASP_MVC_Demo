044594da1c8dc0f7b1bd50d673efad50fa93aa07
VZGLY <113683718+BS-GavinC@users.noreply.github.com>
2024-01-03 10:31:20 +0100

    Ajoutez des fichiers projet.

---
+++ DevNetCloud/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using DevNetCloud.Models;
+using DevNetCloud.Models.Forms;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DevNetCloud.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RegisterForm user)
+        {
+            if (ModelState.IsValid)
+            {
+                Console.WriteLine(user.Email);
+                Console.WriteLine(user.Password);
+                return View("Index");
+            }
+
+            Console.WriteLine("Formulaire pas bien rempli !");
+
+            return View();
+        }
+    }
+}
---
+++ DevNetCloud/Models/Forms/RegisterForm.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevNetCloud.Models.Forms
+{
+    public class RegisterForm
+    {
+        [EmailAddress]
+        [Required(ErrorMessage = "Le Champ Email est requis")]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+
+    }
+}
